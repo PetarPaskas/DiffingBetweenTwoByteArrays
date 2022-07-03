dc2d4d26a24145ea9aa3972e122610761aafe95b
PetarPaskas <petar.paskas@hotmail.com>
2022-07-03 15:09:58 +0200

    Moving .git source control into root directory from webapi

---
+++ DescartesTests/UnitTests/DiffingHelperTest.cs
@@ -0,0 +1,54 @@
+﻿using Descartes.Helpers;
+using NUnit.Framework;
+using Fake = DescartesTests.UnitTests.DiffingHelperTest_FakeData;
+
+namespace DescartesTests.UnitTests
+{
+    [TestFixture]
+    public class DiffingHelperTest
+    {
+        private DiffingHelper diffingHelper;
+
+        [SetUp]
+        public void Setup()
+        {
+            diffingHelper = new();
+        }
+
+        [Test]
+        public void Compare_LeftArrayIsLonger_ReturnsResponseBasedAroundLeftArray()
+        {
+            //Assume
+            //Act
+            //Assert
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Compare_RightArrayIsLonger_ReturnsResponseBasedAroundRightArray()
+        {
+            //Assume
+            //Act
+            //Assert
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Compare_ArraysAreEqual_ReturnsResponseAsEquals()
+        {
+            //Assume
+            //Act
+            //Assert
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Compare_OneOfTheArraysIsNull_ThrowsException()
+        {
+            //Assume
+            //Act
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
---
+++ DescartesTests/UnitTests/DiffingHelperTest_FakeData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescartesTests.UnitTests
+{
+    public static class DiffingHelperTest_FakeData
+    {
+        /// <summary>
+        /// Gemerates an array of bytes
+        /// </summary>
+        /// <param name="sizeInKB">Specify the number of KB you want your array to have. Default: 1KB</param>
+        /// <returns>Byte array</returns>
+        public static byte[] GenerateFakeArray(int sizeInKB = 1)
+        {
+            byte[] data = new byte[sizeInKB * 1024];
+            new Random().NextBytes(data);
+            return data;
+        }
+
+    }
+}
